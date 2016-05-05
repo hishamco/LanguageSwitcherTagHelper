bfda470c4f3ab7917d1085e4a8f6528f5a3f9cdd
hishamco <hishamco_2007@yahoo.com>
2016-05-06 02:07:29 +0300

    Using string interpolation

--- TagHelpers/LanguageSwitcherTagHelper.cs
+++ TagHelpers/LanguageSwitcherTagHelper.cs
@@ -8,73 +8,73 @@ using Microsoft.Extensions.Options;
 namespace LanguageSwitcherTagHelper.TagHelpers
 {
     [HtmlTargetElement("language-switcher")]
-public class LanguageSwitcherTagHelper : TagHelper
-{
-    private IOptions<RequestLocalizationOptions> _locOptions;
-
-    public LanguageSwitcherTagHelper(IOptions<RequestLocalizationOptions> options)
+    public class LanguageSwitcherTagHelper : TagHelper
     {
-        _locOptions = options;
-    }
+        private IOptions<RequestLocalizationOptions> _locOptions;
 
-    [ViewContext, HtmlAttributeNotBound]
-    public ViewContext ViewContext { get; set; }
-
-    public DisplayMode Mode { get; set; } = DisplayMode.ImageAndText;
-
-    public override void Process(TagHelperContext context, TagHelperOutput output)
-    {
-        var selectedCulture = ViewContext.HttpContext.Features.Get<IRequestCultureFeature>().RequestCulture.Culture;
-        var cultures = _locOptions.Value.SupportedUICultures;
-
-        output.TagName = null;
-
-        switch (Mode)
+        public LanguageSwitcherTagHelper(IOptions<RequestLocalizationOptions> options)
         {
-            case DisplayMode.ImageAndText:
-                output.Content.AppendHtml(@"<ul class='nav navbar-nav navbar-right'>
-                        <li class='dropdown'>
-                            <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'><img src='/images/" + selectedCulture.Name + ".png' /> " + selectedCulture.EnglishName + @"<span class='caret'></span></a>
-                            <ul class='dropdown-menu'>");
-                foreach (var culture in cultures)
-                {
-                    output.Content.AppendHtml($"<li><a href='#' onclick=\"useCookie('{culture.Name}')\"><img src='/images/{culture.Name}.png' /> {culture.EnglishName}</a></li>");
-                }
-                break;
-            case DisplayMode.Image:
-                output.Content.AppendHtml(@"<ul class='nav navbar-nav navbar-right'>
-                        <li class='dropdown'>
-                            <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'><img src='/images/" + selectedCulture.Name + @".png' /> <span class='caret'></span></a>
-                            <ul class='dropdown-menu'>");
-                foreach (var culture in cultures)
-                {
-                    output.Content.AppendHtml($"<li><a href='#' onclick=\"useCookie('{culture.Name}')\"><img src='/images/{culture.Name}.png' /></a></li>");
-                }
-                break;
-            case DisplayMode.Text:
-                output.Content.AppendHtml(@"<ul class='nav navbar-nav navbar-right'>
-                        <li class='dropdown'>
-                            <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'> " + selectedCulture.EnglishName + @"<span class='caret'></span></a>
-                            <ul class='dropdown-menu'>");
-                foreach (var culture in cultures)
-                {
-                    output.Content.AppendHtml($"<li><a href='#' onclick=\"useCookie('{culture.Name}')\">{culture.EnglishName}</a></li>");
-                }
-                break;
+            _locOptions = options;
         }
-        output.Content.AppendHtml(@"</ul>
+
+        [ViewContext, HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public DisplayMode Mode { get; set; } = DisplayMode.ImageAndText;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var selectedCulture = ViewContext.HttpContext.Features.Get<IRequestCultureFeature>().RequestCulture.Culture;
+            var cultures = _locOptions.Value.SupportedUICultures;
+
+            output.TagName = null;
+
+            switch (Mode)
+            {
+                case DisplayMode.ImageAndText:
+                    output.Content.AppendHtml($@"<ul class='nav navbar-nav navbar-right'>
+                        <li class='dropdown'>
+                            <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'><img src='/images/{selectedCulture.Name}.png' /> {selectedCulture.EnglishName}<span class='caret'></span></a>
+                            <ul class='dropdown-menu'>");
+                    foreach (var culture in cultures)
+                    {
+                        output.Content.AppendHtml($"<li><a href='#' onclick=\"useCookie('{culture.Name}')\"><img src='/images/{culture.Name}.png' /> {culture.EnglishName}</a></li>");
+                    }
+                    break;
+                case DisplayMode.Image:
+                    output.Content.AppendHtml($@"<ul class='nav navbar-nav navbar-right'>
+                        <li class='dropdown'>
+                            <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'><img src='/images/{selectedCulture.Name}.png' /> <span class='caret'></span></a>
+                            <ul class='dropdown-menu'>");
+                    foreach (var culture in cultures)
+                    {
+                        output.Content.AppendHtml($"<li><a href='#' onclick=\"useCookie('{culture.Name}')\"><img src='/images/{culture.Name}.png' /></a></li>");
+                    }
+                    break;
+                case DisplayMode.Text:
+                    output.Content.AppendHtml($@"<ul class='nav navbar-nav navbar-right'>
+                        <li class='dropdown'>
+                            <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'> {selectedCulture.EnglishName}<span class='caret'></span></a>
+                            <ul class='dropdown-menu'>");
+                    foreach (var culture in cultures)
+                    {
+                        output.Content.AppendHtml($"<li><a href='#' onclick=\"useCookie('{culture.Name}')\">{culture.EnglishName}</a></li>");
+                    }
+                    break;
+            }
+            output.Content.AppendHtml(@"</ul>
                         </li>
                     </ul>");
 
-        output.Content.AppendHtml(@"<script type='text/javascript'>
+            output.Content.AppendHtml($@"<script type='text/javascript'>
         function useCookie(code) {{
             var culture = code;
             var uiCulture = code;
-            var cookieValue = '" + CookieRequestCultureProvider.DefaultCookieName + @"=c='+code+'|uic='+code; 
+            var cookieValue = '{CookieRequestCultureProvider.DefaultCookieName}=c='+code+'|uic='+code; 
             document.cookie = cookieValue; 
             window.location.reload();
         }}
         </script>");
+        }
     }
-}
 }
